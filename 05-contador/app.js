14d5f0fe74e61259327cf221e71248664fe5586d
aryely-lang <25203777@CFP127ALUNO.LOCAL>
2026-04-06 07:53:15 -0300

    iniciando atividade 06 tabuada

---
+++ 05-contador/app.js
@@ -0,0 +1,32 @@
+'use strict'
+
+//função responsavel por fazer a contagem do indice de 1 em 1
+function contadorSequencial(){
+
+}
+
+//função responsavel por fazer a contagem do indice x2
+function contadorPares(){
+
+}
+
+//função responsavel por fazer a contagem do indice x2 -1
+function contadorImpares(){
+
+}
+
+//função responsavl por fazer a contagem do indice x5
+function contadorMultiplos(){
+
+}
+
+//função responsavl por fazer a contagem do indice 2 ^ (índice - 1
+function contadorPotenciais(){
+
+}
+
+//função responsavel por exibir os resultados
+function exibirResultado(){
+
+
+}
